a631989e5bb0ea834b46f6da687f0afc36bd2ab6
Yiyi Sun <yiyisun@gmail.com>
2019-05-31 10:16:56 -0400

    use generated links if no sidebar

--- src/index.tsx
+++ src/index.tsx
@@ -2,14 +2,14 @@ import './layout/index.css';
 
 import app from 'apprun-site';
 import layout from './layout';
-import pages from './_lib/index';
+import pages, {links} from './_lib/index';
 
 import * as config from './config.json';
 const site = {
   title: config.title,
   element: config.element,
   nav: config.nav,
-  sidebar: config.sidebar,
+  sidebar: config.sidebar && config.sidebar.length || links,
   layout,
   pages,
 };
