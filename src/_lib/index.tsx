1f7eda16bd3a394e0d92daa70774f2fb44bf886f
Yiyi Sun <yiyisun@gmail.com>
2019-06-02 11:16:15 -0400

    simplify

--- src/_lib/index.tsx
+++ src/_lib/index.tsx
@@ -1,19 +1,19 @@
 // this file is auto-generated
-import _README_md_0 from './README_md';
-import _index_1 from './_404/index_tsx';
-import _index_2 from './about/index_tsx';
-import _index_3 from './contact/index_tsx';
-import _index_4 from './index_tsx';
+import _about_0 from './about_tsx';
+import _contact_1 from './contact_tsx';
+import _home_2 from './home_tsx';
+import _README_md_3 from './README_md';
+import _index_4 from './_404/index_tsx';
 export default [
-	["/README", _README_md_0],
-	["/_404", _index_1],
-	["/about", _index_2],
-	["/contact", _index_3],
-	["/", _index_4],
+	["/about", _about_0],
+	["/contact", _contact_1],
+	["/home", _home_2],
+	["/README", _README_md_3],
+	["/_404", _index_4],
 ] as (readonly [string, any])[];
 export const links = [
-	{"link": "/README", "text": "/README"},
 	{"link": "/about", "text": "/about"},
 	{"link": "/contact", "text": "/contact"},
-	{"link": "/", "text": "/"},
+	{"link": "/home", "text": "/home"},
+	{"link": "/README", "text": "/README"},
 ]
