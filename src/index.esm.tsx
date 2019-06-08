77fbc839e35f828352218839dc64e3b4698d60c7
Yiyi Sun <yiyisun@gmail.com>
2019-06-08 17:10:26 -0400

    add dynamic es module loading

--- src/index.esm.tsx
+++ src/index.esm.tsx
@@ -1,12 +1,24 @@
-import app from 'apprun-site';
+import app from 'apprun-site/esm/index-esm';
+
 import layout from './layout/index';
 import pages from './_lib/index-esm';
 
+import './components/counter';
+import './components/my-xkcd';
+
 const site = {
-  title: 'My App',
+  title: 'My AppRun Site',
   element: 'main',
-  nav: [],
-  sidebar: [],
+  nav: [
+    { "text": "Home", "link": "/" },
+    { "text": "Contact", "link": "/contact" },
+    { "text": "About", "link": "/about" }
+  ],
+  sidebar: [
+    { "text": "Home", "link": "/" },
+    { "text": "Contact", "link": "/contact" },
+    { "text": "About", "link": "/about" }
+  ],
   layout,
   pages,
   eventRoot: '/'
