cf25d356705f4b29e7b5345f998197b2dfdbe032
Yiyi Sun <yiyisun@gmail.com>
2022-03-13 14:14:30 -0400

    add functions to app

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import app from 'apprun';
 import site_meta from './site.json';
 window['site_meta'] = site_meta;
 
-window['add_link'] = (rel, href, type) =>  {
+app['add_link'] = (rel, href, type) =>  {
   const link = document.createElement('link');
   rel && (link.rel = rel);
   type && (link.type = type);
@@ -11,7 +11,7 @@ window['add_link'] = (rel, href, type) =>  {
   document.head.appendChild(link);
 };
 
-window['add_css'] = url => new Promise((resolve, reject) => {
+app['add_css'] = url => new Promise((resolve, reject) => {
   const link = document.createElement('link');
   link.rel = "stylesheet";
   link.type = "text/css";
@@ -21,7 +21,7 @@ window['add_css'] = url => new Promise((resolve, reject) => {
   document.head.appendChild(link);
 });
 
-window['add_js'] = (url, type = null) => new Promise((resolve, reject) => {
+app['add_js'] = (url, type = null) => new Promise((resolve, reject) => {
   const link = document.createElement('script') as HTMLScriptElement;
   link.src = url;
   (type) ? link.type = type : null;
@@ -32,18 +32,25 @@ window['add_js'] = (url, type = null) => new Promise((resolve, reject) => {
 
 const { layout, routes, element } = site_meta;
 
-routes.forEach(({ path, component }) => {
-  app.on(path, async () => {
-    const module = await import(`./${component}.js`);
-    new module.default().start(element);
-  });
-});
 
-import(`./${layout}.js`).then(async module => {
-  const { Layout, styles, scripts, body_class } = module.default || {};
-  if (styles) for (let i = 0; i < styles.length; i++) await window['add_css'](styles[i]);
-  if(scripts) for (let i = 0; i < scripts.length; i++) await window['add_js'](scripts[i]);
-  body_class && document.body.classList.add(...body_class);
-  Layout && app.render(document.body, <Layout />);
-  app.run('route', location.hash);
-});
+app['add-route'] = (path, component) => {
+  app.once(path, async () => {
+    const module = await import(`./${component}.js`);
+    new module.default().mount(element);
+  });
+};
+
+routes.forEach(({ path, component }) => app['add-route'] (path, component));
+
+app['set-layout'] = async (layout) => {
+  import(`./${layout}.js`).then(async module => {
+    const { Layout, styles, scripts, body_class } = module.default || {};
+    if (styles) for (let i = 0; i < styles.length; i++) await app['add_css'](styles[i]);
+    if (scripts) for (let i = 0; i < scripts.length; i++) await app['add_js'](scripts[i]);
+    body_class && document.body.classList.add(...body_class);
+    Layout && app.render(document.body, <Layout />);
+    app.run('route', location.hash);
+  });
+};
+
+app['set-layout'](window['layout'] || layout);
