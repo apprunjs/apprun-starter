c156e7e02b2a4e4e5e02c4d8f8c21a8ffd76e190
Yiyi Sun <yiyisun@gmail.com>
2022-03-17 10:47:17 -0400

    add $toggle directive

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,29 @@
+// directives
+import app from 'apprun';
+
+app.on('$', ({ key, props }) => {
+  if (key === '$show') {
+    const show = props[key];
+    props.ref = e => e.style.display = show ? '' : 'none';
+  }
+
+  if (key === '$toggle') {
+    const [el, cls] = props[key];
+    props.onclick = () => {
+      document.querySelector(el).classList.toggle(cls);
+    }
+  }
+});
+
+
+
 // dynamic layout
 // import { load_layout } from './apprun_site';
 // import { layout } from './site.json';
 // load_layout(`${layout}/layout`);
 
 // static layout
-import { render_layout } from './apprun_site';
-import config from './tailwind/layout';
-render_layout(config);
+import { render_layout, load_apprun_dev_tools } from './apprun_site';
+import layout from './tailwind/layout';
+load_apprun_dev_tools();
+render_layout(layout);
